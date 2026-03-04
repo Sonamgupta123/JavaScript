6f75b24adf7555634708eeedeaa13b7338acdfe0
Sonam Gupta <sonamflower34@gmail.com>
2026-03-05 00:53:38 +0530

    adding operator.js file

---
+++ operator.js
@@ -0,0 +1,33 @@
+// Day-2 of JS
+// in this we study operators 
+// Arithmatic op (+,-,/,*,%)
+let a = 5;
+let b = 2;
+console.log("a+b = ", a+b);
+console.log("a**b = ", a**b); //exponentiation op.(5^2 = 25) 
+// unary op.
+
+a++;
+console.log("a = ",a);
+
+//assignment op.
+ 
+a += 4; //a = a+4 
+console.log("a= ",a); 
+
+//comparison op.(==,!= , ===, !== , > ,>= ,< , <=)
+ let c = 6;
+ let d = 7;
+
+ console.log("6==7 " , c==d);
+
+ //logical op. (&& , || ,!)
+  let cond1 = c>d;
+  let cond2 = c===6;
+  console.log("cond1 && cond2 = ",cond1 && cond2);
+  
+  console.log("cond1 && cond2 = ",cond1 || cond2);
+  
+  console.log("!(6>7) = ",!(c>d));
+
+
