4b4d534a909c0d0b0ff17fc499e58ccc77933446
Sonam Gupta <sonamflower34@gmail.com>
2026-03-06 23:38:30 +0530

    updating forloop.js with more concept

--- forloop.js
+++ forloop.js
@@ -3,3 +3,25 @@
 for(let count = 1 ; count<=5 ; count++){
     console.log("Sonam Gupta");
 }
+// sum of 1 to 5
+let sum = 0;
+for(let i=1 ; i<=5;i++){
+    sum = sum +i;
+}
+console.log("sum is = ",sum);
+
+//infinite loop :never end loop
+// While loop
+let j = 1; // initialisation
+while(j<=5){ //condition
+    console.log("j = ",j);
+    j++; //updation
+} 
+
+// do while loop
+ let k=1;
+ do{
+    console.log("k= ",k);
+    k++;
+ }while(k<=5);
+
