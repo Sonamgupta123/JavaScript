2472f62326ba0452e863ebd928d2fd5c30d5cce8
Sonam Gupta <sonamflower34@gmail.com>
2026-03-07 23:56:22 +0530

    adding string.js file

---
+++ string.js
@@ -0,0 +1,21 @@
+// strings
+let str = "sonam gupta";
+console.log(str[1]);
+let x = str.toUpperCase(); // or str = str.toUpperCase()
+console.log(x);
+
+//Template literals
+let specialStr=`This is a template literal ${1+2+3}`; //string uses backtick
+console.log( specialStr);
+
+let obj ={
+    item :"pen",
+    price :10,
+
+};
+
+let output = `the cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output);
+
+let str1 = "0123456";
+console.log(str.slice());
