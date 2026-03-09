2144c74dc0765c2185defc1eaf2f62643b04c3a7
Sonam Gupta <sonamflower34@gmail.com>
2026-03-10 00:03:09 +0530

    updating map.js

--- map.js
+++ map.js
@@ -1,6 +1,12 @@
 // map in JS 
 //map is the imp method in array in js
-let nums =[67,87,90];
-nums.map((val)=>{
-console.log(val);
+// let nums =[67,87,90];
+// nums.map((val)=>{
+// console.log(val);
+// });
+nums =[67,87,90];
+let newArr=  nums.map((val)=>{
+// return val;
+return val*val;
 });
+console.log(newArr);
