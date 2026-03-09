323677c73cc4b10858a1374e600ac4bc3ad28b03
Sonam Gupta <sonamflower34@gmail.com>
2026-03-09 23:40:14 +0530

    adding function2.js

---
+++ function2.js
@@ -0,0 +1,43 @@
+// function countVowels(str){
+//     let count=0;
+//     for (char of str){
+//        if(char=="a"||char=="e"||char=="i"||char=="o"||char=="u"){
+//         console.log(char);
+//         count++;
+//        }
+//     }
+    
+//    return count;
+// }
+
+// console.log(countVowels("hello"));
+
+// same with arrow function
+const countVowels = (str)=>{
+    let count=0;
+    for (char of str){
+       if(char=="a"||char=="e"||char=="i"||char=="o"||char=="u"){
+        console.log(char);
+        count++;
+       }
+    }
+    
+   return count;
+}
+
+console.log(countVowels("hello"));
+
+//for each loop in array
+let arr =[1,2,3,4,5];
+
+// arr.forEach (function printVal(val){
+// console.log(val);
+// });
+// this for each generally written in arrow function
+
+// arr.forEach((val)=>{
+// console.log(val);
+// });
+arr.forEach((val,idx,arr)=>{
+console.log(val,idx,arr);
+});
