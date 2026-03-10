4a1d8ada28cb4b691866a603d1ae115b44f937aa
Sonam Gupta <sonamflower34@gmail.com>
2026-03-10 20:51:38 +0530

    adding dom.js

---
+++ dom.js
@@ -0,0 +1,19 @@
+//Day-6 of JS
+// Concept of Dom
+// console.log("hello");
+// //window.console.log("hello2");// object created by browser not js contains predefined keywords
+// console.dir(document);
+// console.dir(document.head);
+
+let heading = document.getElementById("heading");
+console.dir(heading);
+
+let paras = document.getElementsByClassName("para");
+console.dir(paras);
+console.log(paras);
+
+let buttons = document.getElementsByTagName("button");
+console.dir(buttons);
+
+let elements = document.querySelector("p");
+console.dir(elements);
