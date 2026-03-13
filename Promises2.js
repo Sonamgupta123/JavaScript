a69406ef6c720b07e24e9d771a4f23ac627f99be
Sonam Gupta <sonamflower34@gmail.com>
2026-03-13 21:05:23 +0530

    adding Promises2.js

---
+++ Promises2.js
@@ -0,0 +1,32 @@
+function asyncFunc(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+ console.log("some data1");
+ resolve("success");
+    },4000);
+    });
+}
+function asyncFunc2(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+ console.log("some data2");
+ resolve("success");
+    },4000);
+    });
+}
+
+//promise chaining 
+console.log("fetching data1");
+let p1= asyncFunc();
+p1.then((res)=>{
+    console.log("fetching data2...");
+let p2= asyncFunc2();
+p2.then((res)=>{});
+
+});
+
+// console.log("fetching data2...");
+// let p2= asyncFunc2();
+// p2.then((res)=>{
+// console.log(res);
+// });
