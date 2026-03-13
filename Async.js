84d1764ca459114349535d35479378c0c1b1bc93
Sonam Gupta <sonamflower34@gmail.com>
2026-03-13 21:24:21 +0530

    adding ayns.js

---
+++ Async.js
@@ -0,0 +1,18 @@
+//Day 10 of js
+//async always return promise
+async function hello(){
+    console.log("hello");
+}
+
+function api(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("weather data");
+            resolve(200);
+        },2000);
+    });
+}
+
+async function getWeatherData() {
+    await api();
+}
