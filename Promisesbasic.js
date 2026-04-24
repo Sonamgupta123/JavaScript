237d97cc1375fff823640ef6e3a30a5e02dabe5c
Sonam Gupta <sonamflower34@gmail.com>
2026-04-24 12:00:00 +0530

    add1

--- Promisesbasic.js
+++ Promisesbasic.js
@@ -1,16 +1,27 @@
-const myPromise = new Promise((resolve, reject) => {
-    let success = true;
+// const myPromise = new Promise((resolve, reject) => {
+//     let success = true;
 
-    if (success) {
-        resolve("Task completed successfully!");
-    } else {
-        reject("Task failed!");
-    }
-});
-myPromise
-    .then((result) => {
-        console.log(result);   // success case
-    })
-    .catch((error) => {
-        console.log(error);    // error case
+//     if (success) {
+//         resolve("Task completed successfully!");
+//     } else {
+//         reject("Task failed!");
+//     }
+// });
+// myPromise
+//     .then((result) => {
+//         console.log(result);   // success case
+//     })
+//     .catch((error) => {
+//         console.log(error);    // error case
+//     });
+function fetchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Data mil gaya!");
+        }, 2000);
     });
+}
+
+fetchData()
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
