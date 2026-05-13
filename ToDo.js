e116b9784b666fb393f792b90217b0db5ff7c002
Sonam Gupta <sonamflower34@gmail.com>
2026-05-13 22:22:52 +0530

    one1

--- ToDo.js
+++ ToDo.js
@@ -4,3 +4,15 @@ function addTask(task) {
     tasks.push(task);
     console.log(`Task Added: ${task}`);
 }
+function showTasks() {
+    console.log("\nYour Tasks:");
+    
+    if (tasks.length === 0) {
+        console.log("No tasks available.");
+        return;
+    }
+
+    tasks.forEach((task, index) => {
+        console.log(`${index + 1}. ${task}`);
+    });
+}
